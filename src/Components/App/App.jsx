31edc99fecca27a7ddfda2fff81f49975214f911
reginaguermandi <reginaguermandi@gmail.com>
2023-05-29 20:12:13 -0300

    add header

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,11 @@
+import Header from '../Header/Header'
+
+function App() {
+  return (
+	<div className="mx-auto border-2 border-rose-500 max-w-7xl">
+		<Header />
+	</div>		
+	);
+}
+
+export default App
